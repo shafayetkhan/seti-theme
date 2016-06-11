2e32b5de50b9ec1fc8b6d00caf159ffa2b215ac8
Shafayet Khan <shafayetkhan@gmail.com>
2016-06-11 04:54:06 -0600

    Remove unwanted/ broken faces from seti

--- shafi-seti-theme.el
+++ shafi-seti-theme.el
@@ -1,10 +1,10 @@
-;;; seti-theme.el --- A dark colored theme, inspired by Seti Atom Theme
+;;; shafi-seti-theme.el --- A dark colored theme, inspired by Shafi-Seti Atom Theme
 ;;
 ;;
 ;; Author: Vlad Piersec <vlad.piersec@gmail.com>
 ;; Version: 0.1
 ;; Keywords: themes
-;; URL: https://github.com/caisah/seti-theme
+;; URL: https://github.com/caisah/shafi-seti-theme
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
@@ -25,20 +25,21 @@
 ;;
 ;;; Commentary
 ;;
-;; Dark theme inspired by Atom's Seti 
+;; Dark theme inspired by Atom's Shafi-Seti 
 ;;
-;; Tries to reproduce the feel of https://github.com/jesseweed/seti-ui
+;; Tries to reproduce the feel of https://github.com/jesseweed/shafi-seti-ui
 ;;; Code
 
-(deftheme seti
-  "Seti - A theme inspired by Seti Atom Theme")
+(deftheme shafi-seti
+  "Shafi-Seti - A theme inspired by Shafi-Seti Atom Theme")
 
 (let ((blue "#55B5DB")
       (green "#9FCA56")
       (yellow "#DCCD69")
       (red "#CE4045")
       (purple "#A074C4")
-      (background "#151718")
+      ;;(background "#151718")
+      (background (if window-system "#000" "#262626"))
       (background-2 "#1E2326")      
       (background-3 "#0D1011")
       (background-4 "#101112")
@@ -55,23 +56,23 @@
       (intense-green "#8BE03C"))
     
   (custom-theme-set-faces
-   'seti
+   'shafi-seti
 
    ;; Basics
    `(default ((t (:background ,background :foreground ,text))))
    `(cursor ((t (:background ,input-text :foreground ,background))))
-   `(highlight ((t (:background ,text-highlight))))
+   ;;`(highlight ((t (:background ,text-highlight))))
    `(minibuffer-prompt ((t (:foreground ,dark-blue :weight bold))))
    `(region ((t (:background ,text-region))))
    `(error ((t (:foreground ,red :weight bold :underline (:color ,red :style line)))))
  
-   `(isearch ((t (:background ,background :foreground ,text :box (:line-width 1 :color ,dark-blue) :weight bold))))
-   `(lazy-highlight ((t (:background ,background :foreground ,text-2 :box (:line-width 1 :color ,dark-blue)))))
-   `(mode-line ((t (:foreground ,text :background ,background-3 :underline (:color ,dark-blue :style line)))))
-   `(mode-line-buffer-id ((t (:weight bold :foreground ,yellow))))
-   `(mode-line-emphasis ((t (:weight bold))))
-   `(mode-line-highlight ((t (:box (:line-width 3 :color ,dark-blue)))))
-   `(mode-line-inactive ((t (:weight light :foreground ,text :background ,background-2))))
+   ;; `(isearch ((t (:background ,background :foreground ,text :box (:line-width 1 :color ,dark-blue) :weight bold))))
+   ;;`(lazy-highlight ((t (:background ,background :foreground ,text-2 :box (:line-width 1 :color ,dark-blue)))))
+   ;; `(mode-line ((t (:foreground ,text :background ,background-3 :underline (:color ,dark-blue :style line)))))
+   ;; `(mode-line-buffer-id ((t (:weight bold :foreground ,yellow))))
+   ;; `(mode-line-emphasis ((t (:weight bold))))
+   ;; `(mode-line-highlight ((t (:box (:line-width 3 :color ,dark-blue)))))
+   ;; `(mode-line-inactive ((t (:weight light :foreground ,text :background ,background-2))))
    `(secondary-selection ((t (:background ,background-2))))
    `(trailing-whitespace ((t (:background ,background-3))))
    `(match ((t (:weight bold :foreground ,background :background ,intense-green))))
@@ -100,28 +101,29 @@
    `(show-paren-mismatch ((t (:foreground ,text-2 :underline (:color ,red :style line)))))
 
    ;; Dired
-   `(dired-directory ((t (:foreground ,text :weight extrabold))))
-   `(dired-header ((t (:foreground "white"  :background ,blue :weight bold))))
-   `(dired-ignored ((t (:foreground ,text-3))))
-   `(dired-flagged ((t (:foreground ,red :weight bold))))
-   `(dired-marked ((t (:background ,blue :foreground "white" :weight normal))))
-   `(dired-perm-write ((t (:foreground ,yellow :weight ultra-bold))))
-   `(dired-symlink ((t (:foreground ,light-blue :weight normal))))
-   `(dired-warning ((t (:inherit (font-lock-warning-face)))))
+   ;; `(dired-directory ((t (:foreground ,text :weight extrabold))))
+   ;; `(dired-header ((t (:foreground "white"  :background ,blue :weight bold))))
+   ;; `(dired-ignored ((t (:foreground ,text-3))))
+   ;; `(dired-flagged ((t (:foreground ,red :weight bold))))
+   ;; `(dired-marked ((t (:background ,blue :foreground "white" :weight normal))))
+   ;; `(dired-perm-write ((t (:foreground ,yellow :weight ultra-bold))))
+   ;; `(dired-symlink ((t (:foreground ,light-blue :weight normal))))
+   ;; `(dired-warning ((t (:inherit (font-lock-warning-face)))))
 
    ;; Lines
-   `(linum ((t (:foreground ,text-4  :weight light :height 0.9))))
+   ;;`(linum ((t (:foreground ,text-4  :weight light :height 0.9))))
    `(fringe ((t (:background ,background-3 :foreground ,text-4))))
    `(left-margin ((t (nil))))
    `(hl-line ((t (:background ,background-4)))))
 
   
 (custom-theme-set-variables
- 'seti
+ 'shafi-seti
  
-  `(cursor-type 'bar)
+  ;;`(cursor-type 'bar)
   `(ansi-color-names-vector [ ,background ,red ,green ,yellow ,blue ,purple ,blue ,text])
-  `(ansi-term-color-vector [unspecified ,background ,red ,green ,yellow ,blue ,purple ,blue ,text])))
+  ;; `(ansi-term-color-vector [unspecified ,background ,red ,green ,yellow ,blue ,purple ,blue ,text])
+  ))
  
 
 ;;;###autoload
@@ -130,6 +132,6 @@
   (add-to-list 'custom-theme-load-path
                (file-name-as-directory (file-name-directory load-file-name))))
 
-(provide-theme 'seti)
+(provide-theme 'shafi-seti)
 
-;;; seti-theme.el ends here
+;;; shafi-seti-theme.el ends here
